1655695d6da07d65fe77007670867681a47cfb57
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2025-09-23 21:11:00 +0300

    Add project files.

---
+++ MVC_Shop/Controllers/LanguageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Shop.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult Favorite()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Shop/Controllers/PlanetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Shop.Controllers
+{
+    public class PlanetController : Controller
+    {
+        public IActionResult BestPlaces()
+        {
+            return View();
+        }
+    }
+}
