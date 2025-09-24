e846695673c9a7dd8aa7a4217acc4945d3fc3e6c
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2025-09-24 23:06:21 +0300

    database HW

---
+++ MVC_Shop/Migrations/20250924200436_Products and Categories Filled.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductsandCategoriesFilled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
